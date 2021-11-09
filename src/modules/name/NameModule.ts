4eba45e3b5972f7928d81622a8d79fcfa71149d9
kherzinger-figure <kherzinger@figure.com>
2021-11-09 11:58:52 -0800

    Added TXs to NameModule and stubbed tests.

--- src/modules/name/NameModule.ts
+++ src/modules/name/NameModule.ts
@@ -1,8 +1,12 @@
+import * as jspb from 'google-protobuf';
 import * as grpc from 'grpc';
 
 import { IProvider } from '../../providers/IProvider';
+import { NameRecord } from '../../types';
 import { IQueryClient, QueryClient } from '../../proto/provenance/name/v1/query_grpc_pb';
-import * as name_query_pb from '../../proto/provenance/name/v1/query_pb';
+import * as provenance_name_v1_name_pb from '../../proto/provenance/name/v1/name_pb';
+import * as provenance_name_v1_query_pb from '../../proto/provenance/name/v1/query_pb';
+import * as provenance_name_v1_tx_pb from '../../proto/provenance/name/v1/tx_pb';
 
 export class NameModule {
 
@@ -11,10 +15,14 @@ export class NameModule {
         this.queryClient = new QueryClient(this.provider.network.uri.toString(), grpc.credentials.createInsecure());
     }
 
+    //----------------------------------------------------------------------------------------------------------------------------------------------
+    // Query
+    //----------------------------------------------------------------------------------------------------------------------------------------------
+
     // Reverse lookup of all names bound to a given address
     lookupNamesForAddress(addr: string): Promise<string[]> {
         return new Promise<string[]> ((resolve, reject) => {
-            const req = new name_query_pb.QueryReverseLookupRequest();
+            const req = new provenance_name_v1_query_pb.QueryReverseLookupRequest();
             req.setAddress(addr);
             this.queryClient.reverseLookup(req, (err, res) => {
                 if (err != null) {
@@ -29,7 +37,7 @@ export class NameModule {
     // Resolve the address for a name
     resolveName(name: string): Promise<string> {
         return new Promise<string> ((resolve, reject) => {
-            const req = new name_query_pb.QueryResolveRequest();
+            const req = new provenance_name_v1_query_pb.QueryResolveRequest();
             req.setName(name);
             this.queryClient.resolve(req, (err, res) => {
                 if (err != null) {
@@ -39,6 +47,39 @@ export class NameModule {
                 }
             });
         });
+    }
+
+    //----------------------------------------------------------------------------------------------------------------------------------------------
+    // TX
+    //----------------------------------------------------------------------------------------------------------------------------------------------
+
+    // Bind a name to an address under the given root (parent) name in the Provenance blockchain
+    bindName(name: NameRecord, parent: NameRecord): jspb.Message {
+        const req = (new provenance_name_v1_tx_pb.MsgBindNameRequest())
+            .setRecord((new provenance_name_v1_name_pb.NameRecord())
+                .setName(name.name)
+                .setAddress(name.address)
+                .setRestricted(name.restricted)
+            )
+            .setParent((new provenance_name_v1_name_pb.NameRecord())
+                .setName(parent.name)
+                .setAddress(parent.address)
+                .setRestricted(parent.restricted)
+            );
+
+        return req;
+    }
+
+    // Delete a bound name from the Provenance blockchain
+    deleteName(name: NameRecord): jspb.Message {
+        const req = (new provenance_name_v1_tx_pb.MsgDeleteNameRequest())
+            .setRecord((new provenance_name_v1_name_pb.NameRecord())
+                .setName(name.name)
+                .setAddress(name.address)
+                .setRestricted(name.restricted)
+            );
+
+        return req;
     }
 
     private readonly provider: IProvider;

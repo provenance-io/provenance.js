0809db74273e0f825ffcf91f390253be17484e58
kherzinger-figure <kherzinger@figure.com>
2021-11-16 11:18:38 -0800

    Fixed bug

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,4 @@
 export * from './attribute/AttributeModule';
-
 export * from './marker/MarkerModule';
-
 export * from './metadata/MetadataModule';
-
 export * from './name/NameModule';
-export * from './name/NameModuleExtensions';
